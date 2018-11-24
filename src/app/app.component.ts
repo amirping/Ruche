602b83422d3ede40514e7a73f5bfc876e273796a
Med Amir Saadallah <amirs-m-s@live.com>
2018-11-24 03:56:06 +0100

    landing + start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
-export class AppComponent {
-  title = 'ruche';
+export class AppComponent implements OnInit {
+  title = "ruche";
+  calling = "Hello Star";
+  btnshow = false;
+  ngOnInit() {
+    let step = 0;
+    setInterval(() => {
+      switch (step) {
+        case 1:
+          this.calling = "Hello Star";
+          console.log("start hhh");
+          break;
+        case 2:
+          this.calling = "We was waiting for your";
+          console.log("start hhh");
+          break;
+        case 3:
+          this.calling = "we are looking for shiny star";
+          console.log("start hhh");
+          break;
+        case 4:
+          this.calling = "you are envolved";
+          console.log("start hhh");
+          break;
+        case 5:
+          this.calling = "Welcome in";
+          console.log("start hhh");
+          break;
+      }
+      step++;
+      if (step > 5) {
+        this.btnshow = true;
+      }
+    }, 1000);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { LandingComponent } from "./landing/landing.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, LandingComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FlexLayoutModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
